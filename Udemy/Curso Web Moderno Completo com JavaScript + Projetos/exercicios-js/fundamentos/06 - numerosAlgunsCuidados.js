9f27bb48fba7b29f054b4981f23c88aa17cceb56
Henrincode <spellctro@gmail.com>
2024-06-21 22:10:04 -0300

    Curso javascript

---
+++ Udemy/Curso Web Moderno Completo com JavaScript + Projetos/exercicios-js/fundamentos/06 - numerosAlgunsCuidados.js
@@ -0,0 +1,9 @@
+console.log(7 / 0) // Não se pode dividir um número por 0, vai dar infinito
+console.log("10" / 2) // interpreta texto como número se for só número
+console.log("Show!" * 2) // Retorna NaN "Not a Number", que significa "Não é um Número". 
+
+// console.log(10.toString()) se estiver fora das () da erro
+console.log((10).toString()) // (10) dentro de parenteses funciona
+
+console.log((10.345).toFixed(2))
+console.log(11.12345.toFixed(2))
---
+++ Udemy/Curso Web Moderno Completo com JavaScript + Projetos/exercicios-js/fundamentos/teste.js
@@ -0,0 +1,5 @@
+function textToBinary(text) {
+    return text.toString(2);
+}
+
+console.log(0.1+0.1 + 0.7);
