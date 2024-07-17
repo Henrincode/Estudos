aafb4663c035c3ecbc3af804296c7b9d1ecb4b33
Henrincode <spellctro@gmail.com>
2024-07-17 13:18:20 -0300

    Update 09 - arrowFunction3.js

--- Udemy/Curso Web Moderno Completo com JavaScript + Projetos/exercicios-js/03 - funcao/09 - arrowFunction3.js
+++ Udemy/Curso Web Moderno Completo com JavaScript + Projetos/exercicios-js/03 - funcao/09 - arrowFunction3.js
@@ -1,3 +1,5 @@
+// Muito cuidado ao chamar o this. e atribuir um determinado atributo para esse this.  dentro do escopo de uma função, porque você vai estar mexendo no escopo global, e aparentemente você olhando o código não fica claro que você esta trabalhando no escopo global, mas de fato você estara.
+
 let comparaComThis = function (param) {
     console.log(this === param)
 }
@@ -5,8 +7,14 @@ let comparaComThis = function (param) {
 comparaComThis(global)
 
 const obj = {}
-comparaComThis = comparaComThis.bind(obj)
+comparaComThis = comparaComThis.bind(obj) // usando o .bind() vai fazer o this. apontar para o obj e não para o global
 comparaComThis(global)
 comparaComThis(obj)
 
-let comaparaComThis
+let comparaComThisArrow = param => console.log(this === param)
+comparaComThisArrow(global)
+comparaComThisArrow(module.exports) // (this) também retorna true.
+
+comparaComThisArrow = comparaComThisArrow.bind(obj)
+comparaComThisArrow(obj)
+comparaComThisArrow(module.exports)
