94eac966f2eacda27d9cd317ed3986e5d8f6376d
Henrincode <spellctro@gmail.com>
2024-07-23 12:25:20 -0300

    aula

--- Udemy/Curso Web Moderno Completo com JavaScript + Projetos/exercicios-js/03 - funcao/11-callback1.js
+++ Udemy/Curso Web Moderno Completo com JavaScript + Projetos/exercicios-js/03 - funcao/11-callback1.js
@@ -5,4 +5,4 @@ function imprimir(nome, indice) {
 }
 
 fabricantes.forEach(imprimir) //.forEach vai fazer um loop e imprimir primeiro o nome e depois o indice do array, e a terceira informação é o array inteiro "forEach(nome, indice, array)"
-fabricantes.forEach(fabricantes => console.log(fabricantes))
+fabricantes.forEach(fabricantes => console.log(fabricantes)) // usando função arrow
