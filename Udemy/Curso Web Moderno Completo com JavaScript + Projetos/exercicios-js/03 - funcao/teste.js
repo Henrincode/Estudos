5d0b480650cffdef04e5244974df28b5327464db
Henrique Marques <spellctro@gmail.com>
2024-07-28 19:49:29 -0300

    Update teste.js

--- Udemy/Curso Web Moderno Completo com JavaScript + Projetos/exercicios-js/03 - funcao/teste.js
+++ Udemy/Curso Web Moderno Completo com JavaScript + Projetos/exercicios-js/03 - funcao/teste.js
@@ -1,14 +1,1 @@
-const x = 'Global'
 
-function aa() {
-    const x = 'Local'
-    function bb() {
-        function cc() {
-            return x
-        }
-        return cc()
-    }
-    return bb()
-}
-const minhaFuncao = aa()
-console.log(minhaFuncao)
